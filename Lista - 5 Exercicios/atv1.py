3187f504013440a33ca5ef4af58cd51a809dc6df
KillAlone <killalonecrypto@gmail.com>
2021-09-08 20:41:49 -0300

    Completed

--- Lista - 5 Exercicios/atv1.py
+++ Lista - 5 Exercicios/atv1.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-def soma(num1, num2, num3):
+def sum(num1, num2, num3):
     return num1 + num2 + num3
 
 userNum1 = float(input("Digite o 1º Número: "))
 userNum2 = float(input("Digite o 2º Número: "))
 userNum3 = float(input("Digite o 3º Número: "))
 
-print(soma(userNum1, userNum2, userNum3))
+print(sum(userNum1, userNum2, userNum3))
---
+++ Lista Feriadão/validarCPF.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+TAMANHO_CPF = 11
+
+def validarCpf(cpf: str):
+    
+    if len(cpf) != TAMANHO_CPF:
+        return False
+
+    if cpf in (c * TAMANHO_CPF for c in "1234567890"):
+        return False
+
+    cpf_reverso = cpf[::-1]
+    for i in range(2, 0, -1):
+        cpf_enumerado = enumerate(cpf_reverso[i:], start=2)
+        dv_calculado = sum(map(lambda x: int(x[1]) * x[0], cpf_enumerado)) * 10 % 11
+        if cpf_reverso[i - 1:i] != str(dv_calculado % 10):
+            return False
+    return print("Cpf Válido")
+
+if __name__ == "__main__":
+    
+    user = input("Digite um número de cpf sem ponto e hífen. Tudo junto: ")
+    print(validarCpf(user))
