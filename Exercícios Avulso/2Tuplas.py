52352491298842561835ded88f940092fa7a2b43
KillAlone <killalonecrypto@gmail.com>
2021-09-14 21:23:16 -0300

    Completed

---
+++ Exercícios Avulso/2Tuplas.py
@@ -0,0 +1,10 @@
+paises = ("Brazil", "EUA", "Russia", "China")
+linguas = ("Portugues", "Ingles", "Russo", "Mandarim")
+dic = {}
+
+def dicionario(p, l):
+    for i in range(len(paises)):
+        print(paises[i], linguas[i])
+        i += i
+    
+dic = dicionario(paises, linguas)
