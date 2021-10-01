760d55f5392179fab50f2f6be75605749f2a7e66
LipeMachado <silvalipe765@gmail.com>
2021-09-30 23:47:25 -0300

    Completed

--- Lista 2 - 8 Exercicios/exercicio7.py
+++ Lista 2 - 8 Exercicios/exercicio7.py
@@ -3,12 +3,12 @@
 import numpy as np
 
 def mix(x, y):
-    x[:2]
-    y[2:]
-    concat = np.concatenate(x, y)
-    return concat
+    x1 = x[:len(x)//2]
+    y1 = y[len(y)//2:]
+    return x1, y1
 
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([6, 7, 8, 9, 10])
-print(mix(x, y))
+concat = np.concatenate(mix(x, y))
+print(concat)
