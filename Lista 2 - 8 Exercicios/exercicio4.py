9b9b3b583dac14662c2476f863ba238c25eb768f
LipeMachado <silvalipe765@gmail.com>
2021-09-30 23:00:50 -0300

    Completed

--- Lista 2 - 8 Exercicios/exercicio4.py
+++ Lista 2 - 8 Exercicios/exercicio4.py
@@ -6,10 +6,10 @@ def first_half(array):
     if len(array) % 2 == 0:
         mt = array[:2:]
         return mt
-    else:
+    else:        
         mean = np.mean(array)
         meanR = np.delete(array, int(mean))
-        return meanR[:2:]
+        return meanR
         
 x1 = np.array([1, 2, 3, 4])
 print(first_half(x1))
---
+++ Lista 2 - 8 Exercicios/exercicio8.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+arr = np.array([1, 5, 4, 8, 3, 7]) 
+
+max_element = np.max(arr) 
+min_element = np.min(arr) 
+
+print(f"Elemento máximo da array: {max_element}") 
+print(f"Elemento minimo da array: {min_element}")
