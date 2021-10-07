a1ed9f0558f0987c421fea488fe0b560e081f887
LipeMachado <silvalipe765@gmail.com>
2021-10-06 21:52:22 -0300

    Completed

--- Exercícios Avulso/palavraPalindromo.py
+++ Exercícios Avulso/palavraPalindromo.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+def palindromo(frase):
+    if frase == frase[::-1]:
+        print("É um palíndromo!!!")
+    else:
+        print("Não é um palíndromo!!!")
+
+frase = input("Qual a frase ou palavra? ").upper().replace(" ", "")
+palindromo(frase)
