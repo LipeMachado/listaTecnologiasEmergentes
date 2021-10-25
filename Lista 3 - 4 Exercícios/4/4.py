24cc8def2f0e46ee2f57ef57dd3f1337fe62adcd
LipeMachado <silvalipe765@gmail.com>
2021-10-24 22:41:42 -0300

    Completed

--- Lista 3 - 4 Exercícios/4/4.py
+++ Lista 3 - 4 Exercícios/4/4.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
-abrir = open('alunos.txt', 'r')
+alunos = open("alunos.txt", "r")
 
-print(abrir.read())
+dicAlunoNota = {}
+soma = 0
+maiorNota = 0
+menorNota = 0
+
+for linha in alunos:
+  alunoNota = linha.split(",")
+
+  nomeAluno = str(alunoNota[0])
+  notaAluno = float(alunoNota[1])
+
+  dicAlunoNota.update({nomeAluno: notaAluno})
+
+  soma = soma + (dicAlunoNota[nomeAluno])
+
+  media = (soma / 23)
+  media = f"{media:.2f}"
+
+  valores = dicAlunoNota.values()
+  maiorNota = max(valores)
+  menorNota = min(valores)
+
+print(f"Media: {media}")
+print(f"Maior Nota: {maiorNota}")
+print(f"Menor Nota: {menorNota}")
